65bb4dd0cb09f258575e7ef7ed1343e1010ba44d
Aissame <aissam.bkh@gmail.com>
2022-06-18 14:02:43 +0100

    v1

---
+++ tatouage.py
@@ -0,0 +1,16 @@
+#Author: aissameXyz
+#data: 18/6/2022
+
+class Components:
+    Coefficients = []
+    U = None
+    S = None
+    V = None
+class tatouage:
+    #attributes
+    """
+    :param watermark_path:
+    :param ratio:
+    :param wavelet:
+    :param level:
+    """
